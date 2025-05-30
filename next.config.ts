b02d522c2cb063b04582108ff88274ffcb12faa7
Léo <leoclipet26@gmail.com>
2025-05-30 13:25:20 +0200

    Modified next config and packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   basePath: isProd ? '/Portfolio_Perso' : '',
   assetPrefix: isProd ? '/Portfolio_Perso/' : '',
   images: {
